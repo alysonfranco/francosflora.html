219d4fb527304fc95a92241d4620668434921db5
Alyson Franco <alysonfranco4@gamil.com>
2023-10-08 23:30:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Thanks for visiting! :)");
+
+// let nav = documement.querySelector(".nav-links");
+
+// function toggleNav() {
+//   let nav = documement.getElementById("nav");
+//   nav.classList.toggle("show");
+// }
+
+//   if (nav.style.display === "block") {
+//     nav.style.display = "none";
+//   } else {
+//     nav.style.display = "block";
+//   }
+// }
+
+// // Create a media condition that targets viewports at least 768px wide
+// const mediaQuery = window.matchMedia("(max-width: 768px)");
+// // Check if the media query is true
+// function smallScreen() {
+//     if (mediaQuery.matches) {
+//         toggleNav();
+//         alert("Media Query Matched!");
+//       }
+// }
